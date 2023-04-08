5efec08f1c71e09a40a75bc8875337a532fb1ab3
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2023-04-08 11:03:00 +0700

    feat(sound): added satisfying place effect

--- src/components/GrabbableObject.tsx
+++ src/components/GrabbableObject.tsx
@@ -1,8 +1,9 @@
 import { Card, Grid, Text } from '@nextui-org/react'
 import { useSpring, animated } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import React from 'react'
+import useSound from 'use-sound'
 
 interface Props {
   title: string,
@@ -14,31 +15,32 @@ const GrabbableObject = (props: Props) => {
   const [startX, setStartX] = useState(0)
   const [startY, setStartY] = useState(0)
   const [isDown, setDown] = useState(false)
+  const [placeFX] = useSound('sounds/card-place.mp3')
 
   const bind = useDrag(({ down, movement: [mx, my] }) => {
     setDown(down)
     if (down) {
       api.start({ x: mx + startX, y: my + startY, immediate: down })
     } else {
+      placeFX()
       setStartX(x.get())
       setStartY(y.get())
     }
   })
 
-
   return (
-    <animated.div className='relative' {...bind()} style={{ x, y }}>
-          <Card variant={isDown ? 'shadow' : 'bordered'} >
-            <Card.Header>
-              <Text b>{props.title}</Text>
-            </Card.Header>
-            <Card.Divider />
-            <Card.Body css={{ py: "$10" }}>
-              <Text>
-                {props.body}
-              </Text>
-            </Card.Body>
-          </Card>
+    <animated.div className='relative' {...bind()} style={{ x, y, touchAction: 'none', cursor: 'pointer' }}>
+      <Card variant={isDown ? 'shadow' : 'bordered'}>
+        <Card.Header>
+          <Text b>{props.title}</Text>
+        </Card.Header>
+        <Card.Divider />
+        <Card.Body css={{ py: "$10" }}>
+          <Text>
+            {props.body}
+          </Text>
+        </Card.Body>
+      </Card>
     </animated.div>
   )
 }
